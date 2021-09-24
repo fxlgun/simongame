20322c583a20fb98fd07c0d21e0f4d7fb53d04b5
Falgun <77203052+fxlgun@users.noreply.github.com>
2021-09-24 12:24:17 +0530

    Update game.js

--- game.js
+++ game.js
@@ -6,7 +6,7 @@ var userClickedPattern = [];
 
 var started = false;
 var level = 0;
-$(".btn-success").on("click", function() {
+$(document).on("click", function() {
   if (!started) {
     $("#level-title").text("Level " + level);
     nextSequence();
